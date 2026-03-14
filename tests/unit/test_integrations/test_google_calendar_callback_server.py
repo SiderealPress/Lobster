291550f531c1bd1af2b66f30ed55dc4874cd7ac0
Sahar Massachi <38640558+sayhar@users.noreply.github.com>
2026-03-14 03:43:04 +0000

    security: redact detected secrets (second pass)

--- tests/unit/test_integrations/test_google_calendar_callback_server.py
+++ tests/unit/test_integrations/test_google_calendar_callback_server.py
@@ -72,10 +72,10 @@ _FAKE_CODE = "4/0AfJohXmFakeAuthCode"
 _FUTURE = datetime.now(tz=timezone.utc) + timedelta(hours=1)
 
 _FAKE_TOKEN = TokenData(
-    access_token="fake-acc",
+    access_token="<REDACTED_SECRET>",
     expires_at=_FUTURE,
     scope=f"{SCOPE_READONLY} {SCOPE_EVENTS}",
-    refresh_token="fake-ref",
+    refresh_token="<REDACTED_SECRET>",
 )
 
 
